c5afaa87de46bfd81dbb1add0fc291906d7c80bc
Solo800606 <solo80@inbox.ru>
2020-03-09 20:40:18 +0300

    Added a file with the completed task

---
+++ lesson9.py
@@ -0,0 +1,29 @@
+﻿import math
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+from selenium import webdriver
+import time 
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    button1 = browser.find_element_by_css_selector("[class='trollface btn btn-primary']")
+    button1.click()
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    element = browser.find_element_by_id("input_value")
+    x = element.text
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    
+    button2 = browser.find_element_by_css_selector("[class='btn btn-primary']")
+    button2.click()    
+
+finally:
+    time.sleep(10)
+    browser.quit()
+
